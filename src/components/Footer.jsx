31b52e5dcf4e1c5c151ba1856482d5c2084f6d54
M-Naufal14 <mnaufal@gmail.com>
2026-01-03 11:01:48 +0700

    feat: tambahkan nama di footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ function Footer() {
         <footer className="footer-custom text-white text-center py-3 mt-auto">
             <div className="container">
                 <p className="mb-0">
-                    &copy; {new Date().getFullYear()} NewsPortal. Hak cipta dilindungi.
+                    &copy; {new Date().getFullYear()} NewsPortal. Hak cipta dilindungi. | Dibuat oleh <strong>M Naufal Arkan</strong>
                 </p>
             </div>
         </footer>
